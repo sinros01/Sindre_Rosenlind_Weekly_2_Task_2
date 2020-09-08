f20a0a7722df3fed3aa432d4495e14af0461f4ca
sinros01 <69473399+sinros01@users.noreply.github.com>
2020-09-08 22:48:01 +0200

    Add project files.

---
+++ Sindre_Rosenlind_Weekly_2_Task_2/Sindre_Rosenlind_Weekly_2_Task_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	int number = 100;
+	for (int number = 100; number >= 0; number--) {
+		std::cout << number << std::endl;
+	}
+
+	return 0;
+}
